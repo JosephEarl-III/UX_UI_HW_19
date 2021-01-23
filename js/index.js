677fd597cedbd6de046877126fdc5422837c123a
MojoJojo <30669139+Jfresh725@users.noreply.github.com>
2021-01-22 23:55:34 -0700

    jquery CTA and initial modal build

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 // JavaScript Document
+$(document).scroll(function() {
+	$("#scrollFade").fadeOut("fast");
+});
+
+var Portmodal = document.getElementById("PortModal");
+
+
+var Portbtn = document.getElementById("PortBtn");
+
+
+var CloseBtn = document.getElementsByClassName("close")[0];
+
+
+Portbtn.onclick = function() {
+  Portmodal.style.display = "block";
+}
+
+
+CloseBtn.onclick = function() {
+  Portmodal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+  if (event.target == Portmodal) {
+    Portmodal.style.display = "none";
+  }
+}
